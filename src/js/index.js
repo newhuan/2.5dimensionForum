08126e8dd9886cd74cdc8a7095b9983d4c0f16cc
newhuan <newhuan2017@gmail.com>
2017-04-02 18:46:10 +0800

    complete templete of subject list

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,42 @@
 /**
  * Created by huhanwen on 2017/3/19.
  */
-$('window').on('load',function () {
 
-});
 let $signIn = $('#sign-in');
 let $signUp = $('#sign-up');
 let $2017 = $('#year-2017');
-let $subjectList = $('.subject-container');
+let $subjectList = $('.subject-list');
 let $responses = $('.response');
+let $subjectTemplete = $('#subject-templete')[0];
+let subjectTemplete = $subjectTemplete.innerHTML;
+let year = '2017';
+
+// $subjectList.delegate('.title', 'click')
+
+$('window').ready(function () {
+   // console.log('123')
+    $.ajax({
+        type : 'get',
+        url : '/api/getSucjectsWithYear',
+        data : {
+            "year" : year
+        },
+        success : function (res) {
+            console.log('getSucjectsWithYear', res);
+            let subjects = res.subjects;
+            for(let i = 0, len = subjects.length; i < len; i++) {
+                let temp = subjectTemplete;
+                temp = temp.replace(/\{\{subName\}\}/g, subjects[i].subName);
+                temp = temp.replace(/\{\{clickNum\}\}/g, subjects[i].clickNum);
+                temp = temp.replace(/\{\{commentNum\}\}/g, subjects[i].commentNum);
+                temp = temp.replace(/\{\{id\}\}/g, subjects[i].id);
+                let $subject = $(temp);
+                $('.subject-list').append($subject);
+            }
+        }
+    })
+});
+
 $signIn.on('click',function () {
    // console.log(1) ;
     $.ajax({
@@ -49,44 +77,45 @@ $2017.on('click', function () {
             "year" : "2017"
         },
         success : function (res) {
-            console.log('getSucjectsWithYear', res);
+            console.log('getSucjectsWithYear', res, $subjectTemplete.innerHTML);
+            let subjects = res.subjects;
         }
     })
 });
 
-$subjectList.on('click', function (e) {
-   let evt = e || window.event;
-   let id = $(this).attr("subjectid");
-   console.log('click:id', id);
-   $.ajax({
-       type: "get",
-       url: "/api/subjectClicked",
-       data: {
-           subjectId: id
-       },
-       success: function (res) {
-           console.log('res', res);
-       }
-   })
-    evt.stopPropagation();
-});
-
-$responses.on('click', function (e) {
-   let evt = e || window.event;
-   let id = $(this).attr('subjectid');
-    console.log('comment:id', id);
-    $.ajax({
-        type: 'get',
-        url: "/api/commentAdded",
-        data: {
-            subjectId: id
-        },
-        success: function (res) {
-            console.log('res', res);
-        }
-    });
-    evt.stopPropagation();
-});
+// $subjectList.on('click', function (e) {
+//    let evt = e || window.event;
+//    let id = $(this).attr("subjectid");
+//    console.log('click:id', id);
+//    $.ajax({
+//        type: "get",
+//        url: "/api/subjectClicked",
+//        data: {
+//            subjectId: id
+//        },
+//        success: function (res) {
+//            console.log('res', res);
+//        }
+//    })
+//     evt.stopPropagation();
+// });
+//
+// $responses.on('click', function (e) {
+//    let evt = e || window.event;
+//    let id = $(this).attr('subjectid');
+//     console.log('comment:id', id);
+//     $.ajax({
+//         type: 'get',
+//         url: "/api/commentAdded",
+//         data: {
+//             subjectId: id
+//         },
+//         success: function (res) {
+//             console.log('res', res);
+//         }
+//     });
+//     evt.stopPropagation();
+// });
 
 
 
